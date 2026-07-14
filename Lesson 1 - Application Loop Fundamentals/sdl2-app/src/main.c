b3371b7d05c049963ed5db89b21c6ce2afa7889a
javavirys <javavirys@gmail.com>
2026-07-14 23:39:37 +0300

    Добавить файлы Makefile и исходный код для приложения-скелета; обновить README с описанием урока 1

---
+++ Lesson 1 - Application Loop Fundamentals/sdl2-app/src/main.c
@@ -0,0 +1,113 @@
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+
+#ifdef PLATFORM_PS2
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 448
+#else
+#define SCREEN_WIDTH 1920
+#define SCREEN_HEIGHT 1080
+#endif
+
+int main(int argc, char *argv[])
+{
+  if (SDL_Init(SDL_INIT_VIDEO) < 0)
+  {
+    SDL_Log("SDL Init Error: %s", SDL_GetError());
+
+    return 1;
+  }
+
+#ifdef PLATFORM_PS2
+  SDL_Window *window = SDL_CreateWindow(
+      "SDL2 Lesson 1",
+      SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED,
+      SCREEN_WIDTH,
+      SCREEN_HEIGHT,
+      0);
+
+  if (!window)
+  {
+    SDL_Log("SDL window is null");
+    SDL_Quit();
+
+    return 1;
+  }
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+  if (!renderer)
+  {
+    SDL_Log("SDL Create Renderer Error: %s", SDL_GetError());
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 1;
+  }
+
+#else
+
+  SDL_Window *window = SDL_CreateWindow(
+      "SDL2 Lesson 1",
+      SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED,
+      SCREEN_WIDTH,
+      SCREEN_HEIGHT,
+      SDL_WINDOW_SHOWN);
+
+  if (!window)
+  {
+    SDL_Log("SDL window is null");
+    SDL_Quit();
+
+    return 1;
+  }
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(
+      window,
+      -1,
+      SDL_RENDERER_ACCELERATED);
+
+  if (!renderer)
+  {
+    SDL_Log("SDL Create Renderer Error: %s", SDL_GetError());
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 1;
+  }
+
+  SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
+
+#endif
+
+  bool running = true;
+
+  while (running)
+  {
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event))
+    {
+      if (event.type == SDL_QUIT)
+      {
+        running = false;
+      }
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+
+  SDL_Quit();
+
+  return 0;
+}
