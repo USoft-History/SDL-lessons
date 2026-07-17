58fe59fc3906e845d75210321642e0061677948b
javavirys <javavirys@gmail.com>
2026-07-17 18:03:28 +0300

    Add support for multiple text labels in SDL2 application
    - Implemented main application logic in main.c to initialize SDL2 and TTF, create a window and renderer, and manage multiple text labels.
    - Created text_label.c and text_label.h to define the TextLabel structure and associated functions for creating, updating, rendering, and destroying text labels.
    - Added functionality to display FPS and score labels, with dynamic updates to the FPS label.
    - Ensured proper resource management by freeing surfaces and textures when no longer needed.

--- Lesson 4 - Font and Text/Lesson 4.3 - Multiple Text Labels/sdl2-app/src/main.c
+++ Lesson 4 - Font and Text/Lesson 4.3 - Multiple Text Labels/sdl2-app/src/main.c
@@ -3,6 +3,8 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 
+#include "text_label.h"
+
 #ifdef PLATFORM_PS2
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 448
@@ -11,7 +13,11 @@
 #define SCREEN_HEIGHT 1080
 #endif
 
-const char *const WINDOW_TITLE = "Lesson 4.2 - FPS Counter";
+static const char *const WINDOW_TITLE = "SDL2 Lesson 4.3 - Multiple Text Labels";
+
+static const SDL_Color white = {255, 255, 255, 255};
+static const SDL_Color yellow = {255, 255, 0, 255};
+static const SDL_Color green = {0, 255, 0, 255};
 
 int main(int argc, char *argv[])
 {
@@ -26,7 +32,6 @@ int main(int argc, char *argv[])
   {
     SDL_Log("TTF_Init FAILED: %s\n", TTF_GetError());
 
-    SDL_Quit();
     return 1;
   }
 
@@ -35,11 +40,18 @@ int main(int argc, char *argv[])
   {
     SDL_Log("TTF_OpenFont error: %s\n", TTF_GetError());
 
-    TTF_Quit();
-    SDL_Quit();
+    return 1;
+  }
+
+  TTF_Font *smallFont = TTF_OpenFont("assets/font.ttf", 18);
+  if (!smallFont)
+  {
+    SDL_Log("TTF_OpenFont error: %s\n", TTF_GetError());
 
     return 1;
   }
+
+  TextLabels_Init(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 #ifdef PLATFORM_PS2
   SDL_Window *window = SDL_CreateWindow(
@@ -82,6 +94,8 @@ int main(int argc, char *argv[])
   if (!window)
   {
     SDL_Log("SDL window is null");
+
+    TTF_Quit();
     SDL_Quit();
 
     return 1;
@@ -96,6 +110,8 @@ int main(int argc, char *argv[])
   {
     SDL_Log("SDL Create Renderer Error: %s", SDL_GetError());
     SDL_DestroyWindow(window);
+
+    TTF_Quit();
     SDL_Quit();
 
     return 1;
@@ -107,35 +123,63 @@ int main(int argc, char *argv[])
 
 #endif
 
-  SDL_Color white = {255, 255, 255, 255};
-
   int frameCount = 0;
   int fps = 0;
-
   Uint32 lastFPSUpdate = SDL_GetTicks();
 
   char fpsText[64];
 
-  snprintf(fpsText, sizeof(fpsText), "FPS: %d", fps);
+  TextLabel titleLabel = {
+      .font = mediumFont,
+      .color = white,
+      .baseY = 20,
+      .align = TEXT_ALIGN_CENTER_X};
 
-  SDL_Surface *fpsSurface =
-      TTF_RenderUTF8_Blended(
-          mediumFont,
-          fpsText,
-          white);
+  strcpy(titleLabel.text, WINDOW_TITLE);
 
-  SDL_Texture *fpsTexture =
-      SDL_CreateTextureFromSurface(
-          renderer,
-          fpsSurface);
+  TextLabel_Create(&titleLabel, renderer);
 
-  SDL_Rect fpsRect = {
-      (SCREEN_WIDTH - fpsSurface->w) / 2,
-      (SCREEN_HEIGHT - fpsSurface->h) / 2,
-      fpsSurface->w,
-      fpsSurface->h};
+  // FPS Label
 
-  SDL_FreeSurface(fpsSurface);
+  TextLabel fpsLabel = {
+      .text = "FPS: 0",
+      .font = smallFont,
+      .color = yellow,
+      .baseX = 50,
+      .baseY = 100,
+      .align = TEXT_ALIGN_NONE};
+
+  TextLabel_Create(&fpsLabel, renderer);
+
+  // Label 3
+  // SDL_Surface *surfaceScore =
+  //     TTF_RenderUTF8_Blended(
+  //         smallFont,
+  //         "Score: 100",
+  //         green);
+
+  // SDL_Texture *textureScore =
+  //     SDL_CreateTextureFromSurface(
+  //         renderer,
+  //         surfaceScore);
+
+  // SDL_Rect rectScore = {
+  //     50,
+  //     150,
+  //     surfaceScore->w,
+  //     surfaceScore->h};
+
+  // SDL_FreeSurface(surfaceScore);
+
+  TextLabel scoreLabel = {
+      .text = "Score: 1000",
+      .font = smallFont,
+      .color = green,
+      .baseX = 50,
+      .baseY = 150,
+  };
+
+  TextLabel_Create(&scoreLabel, renderer);
 
   bool running = true;
 
@@ -167,55 +211,28 @@ int main(int argc, char *argv[])
           "FPS: %d",
           fps);
 
-      SDL_Surface *newSurface =
-          TTF_RenderUTF8_Blended(
-              mediumFont,
-              fpsText,
-              white);
-
-      if (!newSurface)
-      {
-        SDL_Log("TTF_RenderUTF8_Blended error: %s",
-                TTF_GetError());
-      }
-      else
-      {
-
-        SDL_DestroyTexture(fpsTexture);
-
-        fpsTexture =
-            SDL_CreateTextureFromSurface(
-                renderer,
-                newSurface);
-
-        fpsRect.w = newSurface->w;
-        fpsRect.h = newSurface->h;
-
-        fpsRect.x =
-            (SCREEN_WIDTH - fpsRect.w) / 2;
-
-        fpsRect.y =
-            (SCREEN_HEIGHT - fpsRect.h) / 2;
-
-        SDL_FreeSurface(newSurface);
-      }
+      TextLabel_SetText(&fpsLabel, renderer, fpsText);
     }
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    SDL_RenderCopy(renderer, fpsTexture, NULL, &fpsRect);
+    TextLabel_Render(renderer, &titleLabel);
+    TextLabel_Render(renderer, &fpsLabel);
+    TextLabel_Render(renderer, &scoreLabel);
 
     SDL_RenderPresent(renderer);
   }
 
-  if (fpsTexture)
-    SDL_DestroyTexture(fpsTexture);
+  TextLabel_Destroy(&titleLabel);
+  TextLabel_Destroy(&fpsLabel);
+  TextLabel_Destroy(&scoreLabel);
 
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
 
   TTF_CloseFont(mediumFont);
+  TTF_CloseFont(smallFont);
 
   TTF_Quit();
   SDL_Quit();
---
+++ Lesson 4 - Font and Text/Lesson 4.3 - Multiple Text Labels/sdl2-app/src/text_label.h
@@ -0,0 +1,49 @@
+#ifndef TEXT_LABEL_H
+#define TEXT_LABEL_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+typedef enum
+{
+    TEXT_ALIGN_NONE,
+    TEXT_ALIGN_CENTER_X,
+    TEXT_ALIGN_CENTER_Y,
+    TEXT_ALIGN_CENTER
+} TextAlign;
+
+typedef struct
+{
+    char text[128];
+
+    SDL_Color color;
+
+    SDL_Texture *texture;
+
+    SDL_Rect rect;
+
+    TTF_Font *font;
+
+    TextAlign align;
+
+    int baseX;
+
+    int baseY;
+} TextLabel;
+
+void TextLabels_Init(int width, int height);
+
+void TextLabel_Create(
+    TextLabel *label,
+    SDL_Renderer *renderer);
+
+void TextLabel_SetText(
+    TextLabel *label,
+    SDL_Renderer *renderer,
+    const char *text);
+
+void TextLabel_Render(SDL_Renderer *renderer, TextLabel *label);
+
+void TextLabel_Destroy(TextLabel *label);
+
+#endif
