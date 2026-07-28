7fe03f33bd053be8534d8fef8b44a9f7a75a5dda
javavirys <javavirys@gmail.com>
2026-07-28 19:10:07 +0300

    Упорядочить создание окна и рендерера: использовать макросы для флагов окна и рендерера

--- Lesson 1 - Application Loop Fundamentals/sdl2-app/src/main.c
+++ Lesson 1 - Application Loop Fundamentals/sdl2-app/src/main.c
@@ -4,9 +4,13 @@
 #ifdef PLATFORM_PS2
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 448
+#define WINDOW_FLAGS 0
+#define RENDERER_FLAGS 0
 #else
 #define SCREEN_WIDTH 1920
 #define SCREEN_HEIGHT 1080
+#define WINDOW_FLAGS SDL_WINDOW_SHOWN
+#define RENDERER_FLAGS SDL_RENDERER_ACCELERATED
 #endif
 
 const char *const WINDOW_TITLE = "SDL2 Lesson 1";
@@ -20,70 +24,34 @@ int main(int argc, char *argv[])
     return 1;
   }
 
-#ifdef PLATFORM_PS2
   SDL_Window *window = SDL_CreateWindow(
       WINDOW_TITLE,
       SDL_WINDOWPOS_CENTERED,
       SDL_WINDOWPOS_CENTERED,
       SCREEN_WIDTH,
       SCREEN_HEIGHT,
-      0);
+      WINDOW_FLAGS);
 
   if (!window)
   {
     SDL_Log("SDL Create Window Error: %s", SDL_GetError());
     SDL_Quit();
-
     return 1;
   }
 
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, RENDERER_FLAGS);
 
   if (!renderer)
   {
     SDL_Log("SDL Create Renderer Error: %s", SDL_GetError());
     SDL_DestroyWindow(window);
     SDL_Quit();
-
     return 1;
   }
 
-#else
-
-  SDL_Window *window = SDL_CreateWindow(
-      WINDOW_TITLE,
-      SDL_WINDOWPOS_CENTERED,
-      SDL_WINDOWPOS_CENTERED,
-      SCREEN_WIDTH,
-      SCREEN_HEIGHT,
-      SDL_WINDOW_SHOWN);
-
-  if (!window)
-  {
-    SDL_Log("SDL Create Window Error: %s", SDL_GetError());
-    SDL_Quit();
-
-    return 1;
-  }
-
-  SDL_Renderer *renderer = SDL_CreateRenderer(
-      window,
-      -1,
-      SDL_RENDERER_ACCELERATED);
-
-  if (!renderer)
-  {
-    SDL_Log("SDL Create Renderer Error: %s", SDL_GetError());
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 1;
-  }
-
+#ifndef PLATFORM_PS2
   SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
-
   SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
-
 #endif
 
   bool running = true;
